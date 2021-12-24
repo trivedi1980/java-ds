6f42eb6c571b93d86c751575052265080eb222ed
Trivedi B <theepi1980@Trivedis-MacBook-Pro.local>
2021-12-24 17:17:10 -0600

    added tp alogos

---
+++ src/main/java/com/learn/ds/tp/TripletSumToZero.java
@@ -0,0 +1,41 @@
+package com.learn.ds.tp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TripletSumToZero {
+    public static void main(String[] args) {
+        System.out.println(TripletSumToZero.searchTriplets(new int[] { -3, 0, 1, 2, -1, 1, -2 }));
+        System.out.println(TripletSumToZero.searchTriplets(new int[] { -5, 2, -1, -2, 3 }));
+    }
+
+    public static List<List<Integer>> searchTriplets(int[] arr) {
+        List<List<Integer>> triplets = new ArrayList<>();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0 && arr[i] == arr[i -1]) continue;
+            
+            int left = i + 1;
+            int right = arr.length - 1;
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+                if (sum == 0) {
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(arr[i]);
+                    triplet.add(arr[left]);
+                    triplet.add(arr[right]);
+                    triplets.add(triplet);
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return triplets;
+    }
+}
